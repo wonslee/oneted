a74cb9ed07fed026d611c0dd2e35dbf3087c5af5
박성준 <seong_jun@jun.local>
2021-07-26 16:17:36 +0900

    [modigy] feature/resume_models

--- resumes/models.py
+++ resumes/models.py
@@ -5,13 +5,14 @@ from users.models       import User
 from jobpostings.models import JobPosting
 
 class Resume(TimeStampModel):
-    user     = models.ForeignKey(User, on_delete=models.CASCADE)
-    title    = models.CharField(max_length=100)
-    is_done  = models.BooleanField(default=False)
-    file_url = models.URLField(null=True)
-    content  = models.JSONField(default=dict)
-    is_file  = models.BooleanField(default=False)
-    applies  = models.ManyToManyField("Apply", through="ResumeApply", related_name="resume")
+    user      = models.ForeignKey(User, on_delete=models.CASCADE)
+    title     = models.CharField(max_length=100)
+    is_done   = models.BooleanField(default=False)
+    file_url  = models.URLField(null=True)
+    content   = models.JSONField(default=dict)
+    is_file   = models.BooleanField(default=False)
+    file_uuid = models.CharField(max_length=200)
+    applies   = models.ManyToManyField("Apply", through="ResumeApply", related_name="resume")
 
     class Meta:
         db_table = "resumes"
